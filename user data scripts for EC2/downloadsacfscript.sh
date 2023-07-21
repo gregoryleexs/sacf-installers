5b558feadff71e3a65894967b8b66e4c1c87ec95
Greg <106865956+gregoryleexs@users.noreply.github.com>
2023-07-21 21:01:36 +0800

    Added script downloader scripts for EC2 userdata field

---
+++ user data scripts for EC2/downloadsacfscript.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+wget https://raw.githubusercontent.com/gregoryleexs/sacf-installers/main/installwebserver.sh -O installwebserver.sh
+wget https://raw.githubusercontent.com/gregoryleexs/sacf-installers/main/installwp.sh -O installwp.sh
---
+++ user data scripts for EC2/downloadsacfscriptamzn.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+wget https://raw.githubusercontent.com/gregoryleexs/sacf-installers/main/installwebserveramzn.sh -O installwebserveramzn.sh
+wget https://raw.githubusercontent.com/gregoryleexs/sacf-installers/main/installwp.sh -O installwp.sh
