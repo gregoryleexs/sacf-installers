dacf3b963044ca41315ba3e4e9ee44e4dc582af4
Greg <106865956+gregoryleexs@users.noreply.github.com>
2023-07-23 22:19:45 +0800

    Added install wget command (not installed by default on RHEL AMI)

--- user data scripts for EC2/downloadsacfscript.sh
+++ user data scripts for EC2/downloadsacfscript.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+sudo dnf install wget -y
 wget https://raw.githubusercontent.com/gregoryleexs/sacf-installers/main/installwebserver.sh -O installwebserver.sh
 wget https://raw.githubusercontent.com/gregoryleexs/sacf-installers/main/installwp.sh -O installwp.sh
